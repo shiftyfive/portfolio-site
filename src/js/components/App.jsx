21e6337041ec1de004ae5d94441cdc1242c72a81
Joshua <joshuawarren000@gmail.coml>
2017-12-05 11:26:49 -0500

    initial setup

---
+++ src/js/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Logo from './Logo';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <Logo />
+      </div>);
+  }
+}
---
+++ src/js/components/Logo.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import style from '../../css/logo.css';
+
+export default class logo extends React.PureComponent {
+  render() {
+    return (
+      <a href="javascript:void(0)" className="logo">
+      <svg width="640" height="480" xmlns="http://www.w3.org/2000/svg">
+      <g>
+       <title>Layer 1</title>
+       <rect id="svg_1" height="12" width="12" y="201.989067" x="261.455393" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect stroke="#000000" id="svg_6" height="12" width="12" y="227.380617" x="281.411704" stroke-width="0" fill="#0beb90"/>
+       <rect id="svg_7" height="12" width="12" y="214.675466" x="281.226169" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_8" height="12" width="12" y="202.012843" x="300.626637" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_9" height="12" width="12" y="202.06801" x="287.400528" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_10" height="12" width="12" y="202.020642" x="274.30841" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_11" height="12" width="12" y="251.166572" x="255.980247" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_12" height="12" width="12" y="257.499471" x="268.93835" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_13" height="12" width="12" y="253.113078" x="281.919315" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_14" height="12" width="12" y="240.23723" x="281.812178" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_2" height="12" width="12" y="245.843605" x="309.692749" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_3" height="12" width="12" y="233.138059" x="304.043543" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_4" height="12" width="12" y="220.167505" x="298.103833" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_5" height="12" width="12" y="232.306976" x="320.73629" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_15" height="12" width="12" y="245.462891" x="331.898779" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_16" height="12" width="12" y="220.016722" x="343.36408" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+       <rect id="svg_17" height="12" width="12" y="232.965367" x="337.544306" stroke-width="0" stroke="#000000" fill="#0beb90"/>
+      </g>
+     </svg>
+      </a>
+    )
+  }
+}
